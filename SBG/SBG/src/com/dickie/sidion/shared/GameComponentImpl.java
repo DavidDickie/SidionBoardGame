00626a4e98d6f39262f6ea916ecaf0371744c797
David Dickie <dave@dickieville.com>
2016-06-16 08:12:26 -0400

    initi checking

---
+++ SBG/SBG/src/com/dickie/sidion/shared/GameComponentImpl.java
@@ -0,0 +1,76 @@
+package com.dickie.sidion.shared;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Observer;
+
+public class GameComponentImpl implements GameComponent, java.io.Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Override
+	public String toString() {
+		return getClass().getName() + attributes;
+	}
+	
+	@Override
+	public String getKey() {
+		return getValue("KEY");
+	}
+	
+	@Override
+	public void setKey(String key) {
+		setValue("KEY", key);
+	}
+
+
+	@Override
+	public Player getOwner(Game game) {
+		return game.getPlayer(attributes.get("PLAYER"));
+	}
+	
+	public void setOwner(Player p) {
+		setValue("PLAYER", p.getKey());
+	}
+
+	@Override
+	public void setValue(String field, String value) {
+		if (!validAttributes.contains(field)){
+			throw new RuntimeException(field + " is not a valid field for " + this.getClass().getName());
+		}
+		attributes.put(field, value);
+	}
+
+	@Override
+	public void selected() {
+		for (Observer o : observers){
+			o.update(null, this);
+		}
+	}
+
+	@Override
+	public String getValue(String field) {
+		if (!validAttributes.contains(field)){
+			throw new RuntimeException(field + " is not a valid field for " + this.getClass().getName());
+		}
+		return attributes.get(field);
+	}
+	
+	public List<String> getKeys(){
+		return validAttributes;
+	}
+	
+	
+	protected Map<String, String> attributes = new HashMap<String, String>();
+	protected List<String> validAttributes = new ArrayList<String>();
+	protected List<Observer> observers = new ArrayList<Observer>();
+	@Override
+	public void addObserver(Observer o) {
+		observers.add(o);
+	}
+
+}
