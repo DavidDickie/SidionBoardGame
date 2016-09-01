de4a7282300b850453053add401c399d96c87b29
David Dickie <dave@dickieville.com>
2016-09-01 17:02:14 -0400

    bids bids bids

--- SBG/SBG/test/com/dickie/sidion/shared/order/BidOrderTest.java
+++ SBG/SBG/test/com/dickie/sidion/shared/order/BidOrderTest.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 
 import org.junit.Test;
 
+import com.dickie.sidion.server.OrderProcessor;
 import com.dickie.sidion.shared.Game;
 import com.dickie.sidion.shared.GameComponent;
 import com.dickie.sidion.shared.Hero;
@@ -26,8 +27,24 @@ public class BidOrderTest {
 		ht.put("GOLD", new Var(4));
 		ht.put("INF", new Var(0));
 		OrderTestUtil.executeOrder(ro, prince, game, ht);
-		System.out.println(prince.getOwner(game).getResource("GOLD") + " " + gold);
+		
+		BidOrder ro2 = new BidOrder();
+		Hero prince2 = game.getHero("Prince_4");
+		game.setGameState(Game.RETREAT);
+		int gold2 = prince2.getOwner(game).getResource("GOLD");
+		HashMap<String, GameComponent> ht2 = new HashMap<String, GameComponent>();
+		ht2.put("MANA", new Var(1));
+		ht2.put("GOLD", new Var(3));
+		ht2.put("INF", new Var(0));
+		OrderTestUtil.executeOrder(ro2, prince2, game, ht2);
+
+		OrderProcessor op = new OrderProcessor();
+		op.doBidOrders(game);
+		
 		assertTrue(prince.getOwner(game).getResource("GOLD") == gold-4);
+		assertTrue(prince2.getOwner(game).getResource("GOLD") == gold2);
+		
+		
 	} 
 
 }
